dc065fde07a73f19f013c38aeaf87af775351ba2
rickyelton <rickyelton@gmail.com>
2025-09-14 13:45:11 -0300

    federation

---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
+
+async function startGateway() {
+  const gateway = new ApolloGateway({
+    supergraphSdl: new IntrospectAndCompose({
+      subgraphs: [
+        { name: 'paciente', url: 'https://cn2x-iryo-paciente-api-49699051626.us-west2.run.app/graphql/' },
+      ],
+    }),
+  });
+
+  const server = new ApolloServer({
+    gateway,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀 Apollo Federation Gateway pronto em ${url}`);
+  console.log('📊 Subgrafos configurados:');  
+}
+
+startGateway().catch(error => {
+  console.error('Erro ao iniciar o gateway:', error);
+  process.exit(1);
+});
