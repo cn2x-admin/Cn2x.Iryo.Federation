25b86d7e5e346d0f7abc2e830ec0173dafb7acbd
henrique_elton <henrique_elton@hotmail.com>
2025-09-19 21:15:19 -0300

    teste

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ async function startGateway() {
   const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
       subgraphs: [
-        { name: 'paciente', url: 'https://cn2x-iryo-paciente-api-49699051626.us-west2.run.app/graphql/' },
-        { name: 'ulcera-venosa', url: 'https://cn2x-iryo-ulceravenosa-api-49699051626.us-west2.run.app/graphql/' },
-        
+        { name: 'paciente', url: 'https://cn2x-iryo-paciente-api-dexulr5xga-wl.a.run.app/graphql' },
+        // { name: 'ulcera-venosa', url: 'https://cn2x-iryo-ulceravenosa-api-dexulr5xga-wl.a.run.app/graphql' }, // Desabilitado: introspection não permitida
       ],
     }),
   });
