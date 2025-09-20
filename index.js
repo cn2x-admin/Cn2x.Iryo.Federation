8755c9a6885afe6fabe82b463c509301b699afa6
henrique_elton <henrique_elton@hotmail.com>
2025-09-19 21:48:53 -0300

    teste

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ async function startGateway() {
     supergraphSdl: new IntrospectAndCompose({
       subgraphs: [
         { name: 'paciente', url: 'https://cn2x-iryo-paciente-api-49699051626.us-west2.run.app/graphql/' },
+        { name: 'ulcera-venosa', url: 'https://cn2x-iryo-ulceravenosa-api-49699051626.us-west2.run.app/graphql/' },
       ],
     }),
   });
-
+//https://cn2x-iryo-ulceravenosa-api-49699051626.us-west2.run.app/
   const server = new ApolloServer({
     gateway,
   });
